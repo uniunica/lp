9f514f25ee2337a6b062c75030edc495cfe8ab01
UniUnica <felipe.silva@uniunica.edu.br>
2025-05-14 17:30:29 -0300

    Comentários

--- scripts/links.js
+++ scripts/links.js
@@ -2,15 +2,15 @@
 const input = document.getElementById("pesquisa-links");
 const cards = document.querySelectorAll("#cards-links a");
 const mensagemSemResultado = document.getElementById("mensagem-sem-resultado");
-
+// Evento para pesquisa de links
 input.addEventListener("input", function () {
   const termo = this.value.toLowerCase();
   let encontrouResultados = false;
-
+    // Busca nos cards
   cards.forEach((card) => {
     const titulo = card.querySelector("h3").textContent.toLowerCase();
     const descricao = card.querySelector("p").textContent.toLowerCase();
-
+    // Verifica se o título ou descrição contém o termo pesquisado
     if (titulo.includes(termo) || descricao.includes(termo)) {
       card.style.display = "block";
       encontrouResultados = true;
@@ -18,7 +18,6 @@ input.addEventListener("input", function () {
       card.style.display = "none";
     }
   });
-
   // Mostra ou esconde a mensagem com base no resultado
   if (encontrouResultados) {
     mensagemSemResultado.classList.add("hidden");
@@ -26,8 +25,7 @@ input.addEventListener("input", function () {
     mensagemSemResultado.classList.remove("hidden");
   }
 });
-// Evento para mostrar mais links
-
+// Variáveis para mostrar mais/menos links
 const verMaisBtn = document.getElementById("ver-mais-btn");
 const recolherBtn = document.getElementById("recolher-btn");
 const linksOcultos = document.querySelectorAll(".hidden-link");
